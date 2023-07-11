f468d868d279ca988cfad9678e81e4b24e66082a
OzanAZamir <ozan470@hotmail.com>
2023-07-11 12:46:04 +0300

    new example added
    Read data from a website

---
+++ ReadDataFromWebsite/ReadDataFromWebsite/Program.cs
@@ -0,0 +1,28 @@
+﻿
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                // Bypass SSL certificate validation
+                System.Net.ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+                string url = "https://chapmanganato.com/manga-dg980989/chapter-1"; // Replace with the actual URL you want to retrieve data from
+
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine("Inner Exception: " + ex.InnerException.Message);
+                }
+            }
+        }
+
+
